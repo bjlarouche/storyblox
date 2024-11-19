c198cae2cf47414527b34123e28bdc74d9f95289
Brandon LaRouche <bjlarouche@verizon.net>
2024-11-19 15:18:17 -0800

    Track story source changes

--- src/packages/ui/storyblox/components/Storyblox.tsx
+++ src/packages/ui/storyblox/components/Storyblox.tsx
@@ -1,5 +1,5 @@
 import Log from "@rbxts/log";
-import React, { useEffect, useState } from "@rbxts/react";
+import React, { useCallback, useEffect, useState } from "@rbxts/react";
 import { ReplicatedStorage } from "@rbxts/services";
 import { DarkTheme, LightTheme, Theme, ThemeProvider } from "@rbxts/uiblox";
 import { RELEASE, STORYBLOX_LOGO, VERSION } from "constants/AppConstants";
@@ -29,6 +29,7 @@ export interface StorybloxProps {
 	logoSrc?: string;
 	version?: string;
 	release?: string;
+	debugEnabled?: boolean;
 }
 
 function Storyblox(props: StorybloxProps) {
@@ -40,6 +41,7 @@ function Storyblox(props: StorybloxProps) {
 		logoSrc = STORYBLOX_LOGO,
 		version = VERSION,
 		release = RELEASE,
+		debugEnabled,
 	} = props;
 
 	const [stories, setStories] = useState<Story[]>([]);
@@ -47,22 +49,30 @@ function Storyblox(props: StorybloxProps) {
 
 	const [theme, setTheme] = useState<Theme>(primaryTheme);
 
-	const addStory = (story: Story) => {
+	const logDebug = useCallback(
+		(message: string) => {
+			if (debugEnabled) {
+				Log.Debug(message);
+			}
+		},
+		[debugEnabled],
+	);
+
+	const trackStory = (story: Story) => {
 		try {
 			const { title } = story;
 
-			if (stories.find((s) => s.title === title)) {
-				Log.Error(`Story ${title} already exists`);
-				error(`Story ${title} already exists`);
-			}
-
 			setStories((oldStories) => {
 				const filteredStories = oldStories.filter((s) => s.title !== title);
+
+				// We are effectively replacing the story if it already exists with the same title, this is to prevent duplicates, but ensure the latest version of the story is used
 				filteredStories.push(story);
 				return filteredStories;
 			});
-		} catch {
-			// Component is unmounting. Do nothing.
+
+			logDebug(`Tracking story: ${title}`);
+		} catch (error) {
+			logDebug(`Issue tracking story ${story.title}: ${error}`);
 		}
 	};
 
@@ -72,19 +82,49 @@ function Storyblox(props: StorybloxProps) {
 				// eslint-disable-next-line @typescript-eslint/no-explicit-any
 				const storyExport = require(root) as StoryExport<any>;
 				const { default: story } = storyExport;
-				addStory(story);
+
+				// If the modulescript source code changes, refresh the story
+				var getPropertyChangedSignalConnection = root.GetPropertyChangedSignal("Source").Connect(() => {
+					logDebug(`Story source updated: ${root.GetFullName()}`);
+
+					const updatedStoryExport = require(root) as StoryExport<any>;
+					const { default: updatedStory } = updatedStoryExport;
+
+					if (updatedStory.title !== story.title) {
+						logDebug(`Story title changed from ${story.title} to ${updatedStory.title}`);
+
+						// Remove old story, if title changed, to prevent stale story from being displayed
+						setStories((oldStories) => {
+							const filteredStories = oldStories.filter((s) => s.title !== story.title);
+							return filteredStories;
+						});
+					}
+
+					// Add updated story
+					trackStory(updatedStory);
+				});
+
+				// Start tracking story
+				trackStory(story);
 
 				// Remove story if root is being removed
 				root.Destroying.Connect(() => {
 					setStories((oldStories) => oldStories.filter((s) => s.title !== story.title));
+
+					// Disconnect signal
+					getPropertyChangedSignalConnection.Disconnect();
+
+					logDebug(`Story removed: ${story.title} because ${root.GetFullName()} was destroyed`);
 				});
 			} catch (error) {
-				Log.Warn(`Issue adding story ${root.Name}...\n ${error}`);
+				logDebug(`Issue loading story from ${root.GetFullName()}: ${error}`);
 			}
 		} else if (VALID_ROOT_TYPES.includes(root.ClassName)) {
 			for (const child of root.GetDescendants()) {
 				findStories(child);
 			}
+		} else {
+			logDebug(`${root.GetFullName()} has invalid root type: ${root.ClassName}`);
 		}
 	};
 
@@ -95,6 +135,7 @@ function Storyblox(props: StorybloxProps) {
 		// Listen for descendants to be added
 		root?.DescendantAdded.Connect((descendant) => {
 			findStories(descendant);
+			logDebug(`Descendant added: ${descendant.GetFullName()} checking for stories`);
 		});
 
 		// Find existing stories
